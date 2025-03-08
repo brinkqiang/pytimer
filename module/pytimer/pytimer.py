fc03357a1fc252dd5fceee7ae48357ab9af22e49
brinkqiang <brink.qiang@gmail.com>
2025-03-08 13:59:53 +0800

    add

--- module/pytimer/pytimer.py
+++ module/pytimer/pytimer.py
@@ -4,20 +4,21 @@ import pytimer
 timer = pytimer.CPyTimer()
 
 def main_idle(id):
-    print(f"{pytimer.gettime()} hello main_idle: {id}")
+    print(f"{pytimer.gettime()} [{id}] hello main_idle")
 
 timer.settimer(1, 1000, main_idle)
 
 print(pytimer.gettime())
 
-# Lua 风格嵌套回调实现
+# 嵌套回调实现
+# warning: sleepms bind IDEvent=0
 timer.sleepms(2000, lambda : (
     (lambda count=[0]: (  # 闭包捕获可变对象
-        print(f"sleepms 2000 {pytimer.gettime()}"),
+        print(f"{pytimer.gettime()} [0] sleepms 2000"),
         timer.settimer(2, 1000, lambda id: (
             (lambda count=count: (
                 count.__setitem__(0, count[0] + 1),
-                print(f"hello world {count[0]}"),
+                print(f"{pytimer.gettime()} [{id}] hello world {count[0]}"),
                 count[0] >= 10 and (
                     timer.killtimer(id),
                     timer.killall(),
